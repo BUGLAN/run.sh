28f8329873369eb73bb69a15f2843a4ca16b4ec1
buglan <1831353087@qq.com>
2020-06-30 11:41:30 +0800

    fix: 调整为函数形式

--- run.sh
+++ run.sh
@@ -6,37 +6,45 @@ tmp=0
 newline=$'\n'
 ok=1
 
-if [ "$1" = "config" ]; then
+_build_in() {
+  if [ "$1" = "config" ]; then
     vim ~/.runrc
     exit 0
 fi
 
-if [ "$1" = "version" ]; then
-  echo "v0.1.0"
-fi
-
-# shellcheck disable=SC2162
-while read line; do
-if [ "[$1]" == "${line}" ]; then
-    tmp=${ok}
-    continue
-fi
-
-# shellcheck disable=SC2166
-if [ -n "${line}" -a "${tmp}" = "${ok}" ]; then
-  if [ -n "${cmd}" ]; then
-    cmd="${cmd}${newline}${line}"
-  else
-    cmd="${line}"
+  if [ "$1" = "version" ]; then
+    echo "v0.2.0"
+    exit 0
   fi
-fi
+}
 
-# shellcheck disable=SC2166
-if [ -z "${line}" -a "${tmp}" = "${ok}" ]; then
-  break
-fi
+_build_cmd() {
+    # shellcheck disable=SC2162
+  while read line; do
+  if [ "[$1]" == "${line}" ]; then
+      tmp=${ok}
+      continue
+  fi
 
-done <${config}
+  # shellcheck disable=SC2166
+  if [ -n "${line}" -a "${tmp}" -eq "${ok}" ]; then
+    if [ -n "${cmd}" ]; then
+      cmd="${cmd}${newline}${line}"
+    else
+      cmd="${line}"
+    fi
+  fi
 
-eval "${cmd}"
-exit 0
+  # shellcheck disable=SC2166
+  if [ -z "${line}" -a "${tmp}" = "${ok}" ]; then
+    break
+  fi
+
+  done <${config}
+  eval "${cmd}"
+  exit 0
+}
+
+_build_in $*
+_build_cmd $*
+
